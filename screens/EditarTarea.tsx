2c0b44e381a366169987772ad52829d77e0793cf
kevincadena95 <kevgeek5@gmail.com>
2026-07-14 10:02:11 -0500

    Actualizar

--- screens/EditarTarea.tsx
+++ screens/EditarTarea.tsx
@@ -1,12 +1,51 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { StyleSheet, Text, View, Button } from 'react-native'
+import React, { useState } from 'react'
+import { TextInput } from 'react-native-gesture-handler'
+import { globalStyles } from '../styles/GlobalStyles'
+import { doc, setDoc, updateDoc } from "firebase/firestore";
+import { db } from '../Firebase/Config';
 
 export default function EditarTarea() {
-  return (
-    <View>
-      <Text>EditarTarea</Text>
-    </View>
-  )
+
+    const [id, setId] = useState(0)
+    const [titulo, setTitulo] = useState("")
+    const [descripcion, setDescripcion] = useState("")
+
+    function editarTarea() {
+
+        updateDoc(doc(db, "tareas", id.toString() ), {
+                    titulo: titulo,
+                    descripcion: descripcion,
+                    
+                });
+
+    }
+
+
+    return (
+        <View>
+            <Text>Guardar Tarea</Text>
+
+            <TextInput placeholder='Ingresa ID de la tarea'
+                style={globalStyles.input}
+                onChangeText={(texto) => setId(+texto)}
+                value={id.toString()} />
+
+            <TextInput placeholder='Ingresa el Titulo de la tarea'
+                style={globalStyles.input}
+                onChangeText={(texto) => setTitulo(texto)}
+                value={titulo} />
+
+            <TextInput placeholder='Ingresa la Descripción de la tarea'
+                style={globalStyles.input}
+                onChangeText={(texto) => setDescripcion(texto)}
+                value={descripcion} />
+
+            <Button title='Guardar'
+                color="blue"
+                onPress={() => editarTarea()} />
+        </View>
+    )
 }
 
 const styles = StyleSheet.create({})
--- screens/LeerTarea.tsx
+++ screens/LeerTarea.tsx
@@ -1,12 +1,50 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { StyleSheet, Text, View, FlatList } from 'react-native'
+import React, { useEffect, useState } from 'react'
+import { doc, getDoc, collection, getDocs } from "firebase/firestore";
+import { db } from '../Firebase/Config';
+
 
 export default function LeerTarea() {
-  return (
-    <View>
-      <Text>LeerTarea</Text>
-    </View>
-  )
+
+    const [tareas, setTareas] = useState([])
+    const [id, setId] = useState(0)
+    const [tarea, setTarea] = useState()
+
+    useEffect(() => {
+        leerTareas()
+    }, [])
+    
+
+    async function leerTareas() {
+
+        const querySnapshot = await getDocs(collection(db, "tareas"));
+        querySnapshot.forEach((doc) => {
+            
+        });
+    }
+
+
+
+
+    return (
+        <View>
+            <Text>Leer Tarea</Text>
+
+            <Text>Lista de Tareas:</Text>
+            <FlatList 
+            data={tareas}
+            renderItem={({item}: {item: any})=>(
+                <View>
+                    <Text style={{ fontSize: 20 }}>{item.id}</Text>
+                    <Text style={{ fontSize: 20 }}>{item.titulo}</Text>
+                    <Text style={{ fontSize: 20 }}>{item.descripcion}</Text>
+                </View>
+            )
+        }
+            />
+
+        </View>
+    )
 }
 
 const styles = StyleSheet.create({})
