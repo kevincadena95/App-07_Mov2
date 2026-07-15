5871fb51dc876c93db73ea9380a51e23834a0cec
kevincadena95 <kevgeek5@gmail.com>
2026-07-15 18:04:08 -0500

    Eliminar Tarea

--- screens/EliminarTarea.tsx
+++ screens/EliminarTarea.tsx
@@ -1,12 +1,40 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { Alert, Button, StyleSheet, Text, TextInput, View } from 'react-native'
+import React, { useState } from 'react'
+import { db } from '../Firebase/Config'
+import { deleteDoc, doc } from 'firebase/firestore'
+import { globalStyles } from '../styles/GlobalStyles'
 
 export default function EliminarTarea() {
-  return (
-    <View>
-      <Text>EliminarTarea</Text>
-    </View>
-  )
+
+   function eliminarTarea() {
+        Alert.alert('Peligro',"Deseas Borrar?", [
+            {
+                text:"Borrar",
+                onPress:async ()=> await deleteDoc(doc(db, "tareas", idTarea))
+            },
+            {
+                text:"Cancelar"
+            }
+        ])
+        
+    }
+
+    const [idTarea, setIdTarea] = useState("")
+
+    return (
+        <View>
+            <Text>EliminarScreen</Text>
+            <TextInput placeholder='Ingresar el ID de la tarea a eliminar'
+                style={globalStyles.input}
+                onChangeText={setIdTarea} />
+
+            <Button
+                onPress={eliminarTarea}
+                title='Eliminar'
+                color={"red"} />
+
+        </View>
+    )
 }
 
 const styles = StyleSheet.create({})
